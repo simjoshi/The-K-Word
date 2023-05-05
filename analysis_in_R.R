23f28fe2b23cbc75739e757e1ddfd776ba39cc91
Simran <simran.joshi942@gmail.com>
2023-05-05 22:52:47 +0530

    Init

--- analysis_in_R.R
+++ analysis_in_R.R
@@ -1,17 +1,47 @@
 #Reading the data
+library(readr)
+data = read_csv('C:/Users/simra/OneDrive/Documents/Docs/Projects/The K-Word/baby_names.csv')
+View(data)
+
+
 nrow(data)
 ncol(data)
 head(data)
 tail(data)
 str(data)
 summary(data)
-
+ 
 #Taking care of missing values
 colSums(is.na(data))
 #No missing values
 
 #Data Visualization
 library(ggplot2)
-ggplot(data, aes(State, Count))+geom_bar(stat="identity")
 
+#Number of baby names from each state
+x <- ggplot(data, aes(x= reorder(State, +Count), y = Count)) + geom_bar(stat="identity")
+x
 ggplot(data, aes(State, Count))+geom_col()
+
+#Number of baby names over time
+ggplot(data, aes(Year, Count)) + geom_line(aes(group=Year))
+
+ggplot(data = data) + 
+  geom_line(mapping = aes(x=Year, y=Count))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
